c47157cec9b9a3ab3bf338a3ab94235064825541
Nathan Chan <nathanchan@Nathans-MacBook-Air.local>
2019-08-21 09:11:30 -0700

    ready for deployment

--- src/server.ts
+++ src/server.ts
@@ -11,6 +11,7 @@ const app = express();
 app.use(express.urlencoded({extended: false}));
 app.use(express.json()); //? Just a configuration for the body parser. Both will result in information in req.body
 app.use(helmet());
+app.use(express.static(__dirname + '/../client/build'))
 
 // const loginLimiter = new RateLimit({
 //     windowMs: 5*60*1000,
@@ -48,6 +49,10 @@ app.use('/restaurants', restaurantRouter);
 import reviewRouter from './routes/review';
 // app.use('/reviews', reviewRouter);
 app.use('/reviews', expressJWT({secret: process.env.JWT_SECRET}), reviewRouter);
+
+app.get("*", (req, res) => {
+    res.sendFile("index.html")
+})
 
 app.listen(process.env.PORT, () => {
     console.log(`You're listening to port ${process.env.PORT}...`)
